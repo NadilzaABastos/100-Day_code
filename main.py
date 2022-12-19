b3357ae7ac83f8e9e70328d7c8453bba96da94da
NadilzaABastos <86935845+NadilzaABastos@users.noreply.github.com>
2022-12-19 16:58:08 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
-from question_model import Question
-from data import question_data
-from quiz_brain import QuizBrain
-# write a for loop to iterate over the questiom_data
-# Create a Question object from each entry question_data
-# Append each Question object to the question_bank
+from turtle import Turtle, Screen
+from snake import Snake
+import time
 
-question_bank = []
+screen = Screen()
+screen.setup(width=600,height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
 
-for question in question_data:
-    new_text = question["text"]
-    new_answer = question["answer"]
-    new_question = Question(new_text,new_answer)
-    question_bank.append(new_question)
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 
-quiz = QuizBrain(question_bank)
-while quiz.still_has_questions():
-    quiz.next_question()
+game_start = True
 
-print("you've complete the quiz ")
-print(f"Your final score was : {quiz.score} /{quiz.question_number}")
+while game_start:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+
+screen.exitonclick()
+
---
+++ snake.py
@@ -0,0 +1,47 @@
+from turtle import Turtle, Screen
+position = [(0,0),(-20,0),(-40,0)]
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+class Snake:
+
+    def __init__(self):
+        self.all_turtles = []
+        self.create_snake()
+        self.head = self.all_turtles[0]
+
+    def create_snake(self):
+        for turtles in position:
+            new_turtle = Turtle("square")
+            new_turtle.color("white")
+            new_turtle.penup()
+            new_turtle.goto(turtles)
+            self.all_turtles.append(new_turtle)
+
+    def move(self):
+        for conjuto in range(len(self.all_turtles) - 1, 0, -1):
+                new_x = self.all_turtles[conjuto - 1].xcor()
+                new_y = self.all_turtles[conjuto - 1].ycor()
+                self.all_turtles[conjuto].goto(new_x, new_y)
+        self.head.forward(20)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(RIGHT)
+
+
+
