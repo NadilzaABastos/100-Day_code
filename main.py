b275d30a6396ee07d91e4e3101d7d6083b1d4c2c
NadilzaABastos <86935845+NadilzaABastos@users.noreply.github.com>
2023-01-04 13:37:36 -0300

    projeto_day_26

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
 import pandas
 
-data = pandas.read_csv ("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
-grey_s = len(data[data["Primary Fur Color"] == "Gray"])
-cinnamon_s= len(data[data["Primary Fur Color"] == "Cinnamon"])
-black_s = len(data[data["Primary Fur Color"] == "Black"])
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+alphabet = {row.letter: row.code for (index,row) in data.iterrows()}
+name = input(str("Type a word :")).upper()
+new_list = [alphabet[letter] for letter in name]
+print(new_list)
 
-new_census = {
-    "Fun Color" : ["Gray", "Cinnamon", "Black"],
-    "Count": [grey_s, cinnamon_s, black_s]
-
-}
-
-new_frame = pandas.DataFrame(new_census)
-new_frame.to_csv("squirrel_count.csv")
-print(new_frame)
